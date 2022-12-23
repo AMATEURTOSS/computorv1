e40f9ff4a2e3ab541ca74c0c63f79ed66d1ec72f
yochoi <amateur.toss@gmail.com>
2022-12-23 15:22:35 +0900

    refactor(main): refactor main.ts

--- srcs/main.ts
+++ srcs/main.ts
@@ -2,4 +2,7 @@ import { Equation } from "./classes";
 import { parseArgs } from "./parse";
 
 const [lhs, rhs] = parseArgs(process.argv);
-console.log(new Equation(lhs, rhs).reduce().toString());
+
+let equation = new Equation(lhs, rhs);
+equation = equation.reduce();
+console.log(equation.toString());
