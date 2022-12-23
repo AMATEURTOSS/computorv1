7505f6919ccbaf34eb98f648013d13970b70c21f
yochoi <amateur.toss@gmail.com>
2022-12-23 22:19:48 +0900

    feat(math): add quadratic method

--- srcs/classes/math/math.ts
+++ srcs/classes/math/math.ts
@@ -1,4 +1,7 @@
 export class Math {
+  /**
+   * @name abs
+   */
   static abs(num: number): number {
     return num < 0 ? -num : num;
   }
@@ -19,5 +22,17 @@ export class Math {
       ret = (tmp + num / tmp) / 2;
     }
     return ret;
+  }
+
+  /**
+   * @name quadratic
+   * @description Solve a quadratic equation
+   */
+  static quadratic(a: number, b: number, c: number): number[] {
+    const delta = b * b - 4 * a * c;
+    if (delta < 0) return [];
+    if (delta === 0) return [-b / (2 * a)];
+    const sqrtDelta = this.sqrt(delta);
+    return [(-b + sqrtDelta) / (2 * a), (-b - sqrtDelta) / (2 * a)];
   }
 }
