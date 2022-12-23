8753367eda817420886e5432344446ce6736b5eb
yochoi <amateur.toss@gmail.com>
2022-12-23 15:39:53 +0900

    test(term): add term test

---
+++ srcs/classes/term/term.spec.ts
@@ -0,0 +1,20 @@
+import { TooHighExponentError } from "../../error";
+import { Term } from "./term";
+
+describe("Term constructor", () => {
+  test("should work with number", () => {
+    const term = new Term(3, 2);
+    expect(term.coefficient).toBe(3);
+    expect(term.exponent).toBe(2);
+  });
+
+  test("should work with string", () => {
+    const term = new Term("1 * x ^ 0");
+    expect(term.coefficient).toBe(1);
+    expect(term.exponent).toBe(0);
+  });
+
+  test("should throw an error with big exponent", () => {
+    expect(() => new Term("1 * x ^ 3")).toThrow(TooHighExponentError);
+  });
+});
