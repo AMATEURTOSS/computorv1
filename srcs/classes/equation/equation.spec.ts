9facc4fa607e2762626dd200935c0575930e646f
yochoi <amateur.toss@gmail.com>
2022-12-24 20:30:01 +0900

    test(equation): add solveEquation() test

--- srcs/classes/equation/equation.spec.ts
+++ srcs/classes/equation/equation.spec.ts
@@ -20,3 +20,17 @@ describe("Equation toString", () => {
     expect(equation.toString()).toBe(`(4${x}${powerOf[0]}) + (4${x}${powerOf[1]}) + (-9.3${x}${powerOf[2]}) = 0`);
   });
 });
+
+describe("Equation solve method", () => {
+  const testSolution = (lhs: string, rhs: string, expected: Array<string>) => {
+    const equation = new Equation(lhs, rhs);
+    const solution = equation
+      .solveEquation()
+      .sort()
+      .map((solution) => solution.toFixed(2));
+    expect(solution).toEqual(expected);
+  };
+  it("Solve1", () => testSolution("3 * X^1 - 42 * X^2", "0", ["0.00", "0.07"]));
+  it("Solve2", () => testSolution("5 * X^0 + 4 * X^1 - 9.3 * X^2", "1 * X^0", ["-0.48", "0.91"]));
+  it("Solve3", () => testSolution("5 * X^0 + 4 * X^1", "4 * X^0", ["-0.25"]));
+});
