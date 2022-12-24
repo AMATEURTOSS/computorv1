09080dd830c497ac1fef03469003bfd5ab2d706e
yochoi <amateur.toss@gmail.com>
2022-12-24 13:24:22 +0900

    feat(main): add error handler

--- srcs/main.ts
+++ srcs/main.ts
@@ -1,11 +1,16 @@
 import { Equation } from "./classes";
 import { parseArgs } from "./parse";
 
-const [lhs, rhs] = parseArgs(process.argv);
+try {
+  const [lhs, rhs] = parseArgs(process.argv);
 
-let equation = new Equation(lhs, rhs);
-const reduced = equation.reduce();
+  let equation = new Equation(lhs, rhs);
+  const reduced = equation.reduce();
 
-console.log(`Reduced form: ${reduced.toString()}`);
-console.log(`Polynomial degree: ${reduced.lhs.length - 1}`);
-console.log(`Solution:\n${reduced.solveEquation().join("\n")}`);
+  console.log(`Reduced form: ${reduced.toString()}`);
+  console.log(`Polynomial degree: ${reduced.lhs.length - 1}`);
+  console.log(`Solution:\n${reduced.solveEquation().join("\n")}`);
+} catch (e) {
+  if (e instanceof Error) console.error(`Error: ${e.message}`);
+  else console.error(e);
+}
